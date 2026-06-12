dccca79c2a660f5589efd8a4c42bee88e66aec00
Himanshu Vaid <himanshuvaid562@gmail.com>
2026-06-12 19:58:20 +0530

    Converted emp details to normal folder

---
+++ emp details/emp details.java
@@ -0,0 +1,8 @@
+class Emp{
+	public static void main(){
+		System.out.println("Emp id: 12");
+		System.out.println("Emp name: Devil");
+		System.out.println("Department: CSE");
+		System.out.println("Updated files");
+	}
+}
